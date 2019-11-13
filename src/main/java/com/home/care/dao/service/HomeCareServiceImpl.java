2e6f5e97dffdc024c259d6fd75ae816b20a69e53
Unknown <unknown@example.com>
2019-11-13 09:24:03 +0530

    exception handling

--- src/main/java/com/home/care/dao/service/HomeCareServiceImpl.java
+++ src/main/java/com/home/care/dao/service/HomeCareServiceImpl.java
@@ -3,18 +3,29 @@ package com.home.care.dao.service;
 import java.util.List;
 
 import org.apache.commons.lang3.math.NumberUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.home.care.bo.FacilityBO;
 import com.home.care.dao.db.HomeCareDbImpl;
 
 public class HomeCareServiceImpl implements HomeCareService{
-
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired HomeCareDbImpl	homeCareDbDao;
 	
 	public List<FacilityBO>	 getFacilities(String facilityType, String state, String zip)
 	{
-		return homeCareDbDao.getFacilities(facilityType, state, zip);
+		List<FacilityBO> facilityBOs	=	null;
+		try
+		{
+			facilityBOs	=	homeCareDbDao.getFacilities(facilityType, state, zip);
+		}
+		catch (Exception ex) {
+			logger.error("Error in fatching facility:  details : "+ex.getMessage());
+			ex.printStackTrace();
+		}
+		return facilityBOs;
 	}
 
 	public  boolean isNumeric(final String str) {
